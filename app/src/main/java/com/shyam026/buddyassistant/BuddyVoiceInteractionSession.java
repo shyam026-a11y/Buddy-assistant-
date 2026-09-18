d39c3e8e31d97157e5bae3093e83ee04f9072052
shyam026-a11y <shyamcreation026@gmail.com>
2026-09-19 03:05:28 +0530

    Fix voice interaction session callback

--- app/src/main/java/com/shyam026/buddyassistant/BuddyVoiceInteractionSession.java
+++ app/src/main/java/com/shyam026/buddyassistant/BuddyVoiceInteractionSession.java
@@ -1,13 +1,16 @@
 package com.shyam026.buddyassistant;
 
+import android.content.Context;
 import android.content.Intent;
-import android.os.Bundle;
 import android.service.voice.VoiceInteractionSession;
 
 public class BuddyVoiceInteractionSession extends VoiceInteractionSession {
-    public BuddyVoiceInteractionSession(android.content.Context context){ super(context); }
+    public BuddyVoiceInteractionSession(Context context){
+        super(context);
+    }
 
-    @Override public void onHandleAssist(Bundle data){
+    @Override public void onShow(android.os.Bundle args,int showFlags){
+        super.onShow(args,showFlags);
         try{
             Intent i=new Intent(getContext(),MainActivity.class);
             i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_SINGLE_TOP);
