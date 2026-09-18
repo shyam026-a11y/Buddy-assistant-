20fdc58a728ba9b87fd695c7f59c8443f4ef15db
shyam026-a11y <shyamcreation026@gmail.com>
2026-09-19 03:05:23 +0530

    Fix voice interaction session service signature

--- app/src/main/java/com/shyam026/buddyassistant/BuddyVoiceInteractionSessionService.java
+++ app/src/main/java/com/shyam026/buddyassistant/BuddyVoiceInteractionSessionService.java
@@ -1,10 +1,11 @@
 package com.shyam026.buddyassistant;
 
+import android.os.Bundle;
 import android.service.voice.VoiceInteractionSession;
 import android.service.voice.VoiceInteractionSessionService;
 
 public class BuddyVoiceInteractionSessionService extends VoiceInteractionSessionService {
-    @Override public VoiceInteractionSession onNewSession(){
+    @Override public VoiceInteractionSession onNewSession(Bundle args){
         return new BuddyVoiceInteractionSession(this);
     }
 }
