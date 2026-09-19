283a650f6852a8e2703a48dcb0847c60ffb96a45
shyam026-a11y <shyamcreation026@gmail.com>
2026-09-19 17:45:55 +0530

    Add repeated wake routing regression coverage

--- app/src/test/java/com/shyam026/buddyassistant/CommandRouterTest.java
+++ app/src/test/java/com/shyam026/buddyassistant/CommandRouterTest.java
@@ -1,6 +1,7 @@
 package com.shyam026.buddyassistant;
 
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class CommandRouterTest {
@@ -14,17 +15,51 @@ public class CommandRouterTest {
     @Test public void wakePhraseRemainderIsCorrect() {
         assertEquals("", CommandRouter.removeWakePhrase("Buddy"));
         assertEquals("", CommandRouter.removeWakePhrase("Hey Buddy"));
-        assertEquals("open youtube", CommandRouter.removeWakePhrase("Buddy open YouTube"));
-        assertEquals("open youtube", CommandRouter.removeWakePhrase("Hey Buddy open YouTube"));
+        assertEquals("open youtube",
+                CommandRouter.removeWakePhrase("Buddy open YouTube"));
+        assertEquals("open youtube",
+                CommandRouter.removeWakePhrase("Hey Buddy open YouTube"));
+        assertNull(CommandRouter.removeWakePhrase("open youtube"));
     }
 
     @Test public void youtubeAndSearchParsing() {
-        assertEquals("arijit singh", CommandRouter.youtubeQuery("youtube search arijit singh"));
-        assertEquals("jee physics", CommandRouter.googleSearchQuery("search JEE physics"));
+        assertEquals(
+                "arijit singh",
+                CommandRouter.youtubeQuery(
+                        "youtube search arijit singh"));
+
+        assertEquals(
+                "jee physics",
+                CommandRouter.googleSearchQuery(
+                        "search JEE physics"));
     }
 
     @Test public void memoryParsing() {
-        assertEquals("my study time is 7 pm", CommandRouter.memoryText("remember my study time is 7 pm"));
-        assertNull(CommandRouter.memoryText("open youtube"));
+        assertEquals(
+                "my study time is 7 pm",
+                CommandRouter.memoryText(
+                        "remember my study time is 7 pm"));
+
+        assertNull(
+                CommandRouter.memoryText(
+                        "open youtube"));
+    }
+
+    @Test public void wakeRoutingRemainsStableAcross1000Repetitions() {
+        for (int i = 0; i < 1000; i++) {
+            assertEquals(
+                    "open youtube",
+                    CommandRouter.removeWakePhrase(
+                            "Hey Buddy open YouTube"));
+
+            assertEquals(
+                    "search jee physics",
+                    CommandRouter.removeWakePhrase(
+                            "Buddy search JEE physics"));
+
+            assertNull(
+                    CommandRouter.removeWakePhrase(
+                            "open youtube"));
+        }
     }
 }
